ef1e5cc3a284a0034e8872cd9192d340016118c2
Promal <promal.barua.cse@gmail.com>
2023-10-10 20:56:50 +0600

    Initial Commit

---
+++ ARKIT Basic/ARKIT_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKIT_BasicApp.swift
+//  ARKIT Basic
+//
+//  Created by Promal on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ARKIT_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
